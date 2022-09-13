b75651db4da579c53da000c5b33b68981e0a9286
Nicolas <nicolas@racchi.com>
2022-09-13 23:27:50 +0200

    feat: progress indicator

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 use csv::{Error, StringRecord};
-use ethers::utils::keccak256;
+use ethers::{
+    abi::{encode, ethabi::Bytes, Token},
+    types::H160,
+    utils::{hex, keccak256, to_checksum},
+};
+use indicatif::{ProgressBar, ProgressState, ProgressStyle};
 use rs_merkle::{Hasher, MerkleTree};
-use std::process;
+use std::{error::Error as StdError, fmt::Write, process};
 
 #[derive(Clone)]
 struct Keccak256 {}
@@ -14,43 +19,56 @@ impl Hasher for Keccak256 {
     }
 }
 
-fn parse_line(line: Result<StringRecord, Error>) -> Result<String, Error> {
+fn parse_leaf(line: Result<StringRecord, Error>) -> Result<Bytes, Error> {
     let line = line?;
-    let address = line.get(1).unwrap().to_string();
+    let address_raw = line.get(1).unwrap()[2..].to_string();
     let label = line.get(2).unwrap().to_string();
 
-    Ok(format!("{}{}", address, label))
+    let address = &hex::decode(address_raw).unwrap();
+    let address = to_checksum(&H160::from_slice(address), None);
+    let address = H160::from_slice(&hex::decode(&address[2..]).unwrap());
+
+    // return solidity abi-encoded leaf
+    Ok(encode(&[Token::Address(address), Token::String(label)]))
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn StdError>> {
     let path = "./data/active_users_audience.csv";
     let time = std::time::Instant::now();
+    let total_size = std::fs::metadata(path)?.len();
+    let line_size = 57;
     let mut count = 0;
 
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(
+        ProgressStyle::with_template(
+            "{spinner:.green} [{elapsed_precise}] [{bar:.cyan/blue}] ({eta})",
+        )
+        .unwrap()
+        .with_key("eta", |state: &ProgressState, w: &mut dyn Write| {
+            write!(w, "{:.0}s", state.eta().as_secs_f64()).unwrap()
+        })
+        .progress_chars("#>-"),
+    );
+
+    // todo: sortPairs?
     let mut tree: MerkleTree<Keccak256> = MerkleTree::new();
 
-    match csv::Reader::from_path(path) {
-        Ok(mut reader) => {
-            for result in reader.records() {
-                let line = parse_line(result).unwrap_or_else(|err| {
-                    eprintln!("Error parsing line: {}", err);
-                    process::exit(1);
-                });
+    let mut reader = csv::Reader::from_path(path)?;
 
-                let hash = Keccak256::hash(line.as_bytes());
-
-                tree.insert(hash);
-                count += 1;
-
-                if count % 100000 == 0 {
-                    println!("{} records processed", count);
-                }
-            }
-        }
-
-        Err(e) => {
-            println!("Error: {}", e);
+    for result in reader.records() {
+        let line = parse_leaf(result).unwrap_or_else(|err| {
+            eprintln!("Error parsing line: {}", err);
             process::exit(1);
+        });
+
+        let hash = Keccak256::hash(line.as_slice());
+
+        tree.insert(hash);
+        count += 1;
+
+        if count % 1000 == 0 {
+            pb.inc(line_size * 1000);
         }
     }
 
@@ -62,6 +80,8 @@ fn main() {
 
     tree.commit();
 
-    println!("Root: {:?} ", tree.root_hex());
+    println!("Root: {} ", tree.root_hex().unwrap());
     println!("Done building Merkle tree in {:?}", time.elapsed());
+
+    Ok(())
 }
